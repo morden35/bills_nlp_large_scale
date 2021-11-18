af7dcb0ae78decafe549d0870e39366141c495dc
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-11-18 14:50:20 -0600

    might be possible to scrape site for bill text. but we could also use their api

---
+++ bill_scrape.py
@@ -0,0 +1,14 @@
+# from spatula import HtmlPage, HtmlListPage, XPath, URL
+
+
+# class CongressList(HtmlListPage):
+#     soruce = ULR("https://www.govinfo.gov/app/collection/bills/117/hr/[0-99]/%7B%22pageSize%22%3A%2220%22%2C%22offset%22%3A%220%22%7D")
+
+#     selector = XPath("//*[@id='browseresultsId']/div/")
+
+
+# congress = [117, 116, 115, 114, 113]
+# session = [1, 2]
+# chamber = ["hr", "s"]
+
+# f"https://www.govinfo.gov/bulkdata/BILLS/{}/{}/{}"
